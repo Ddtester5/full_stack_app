74cfbe2fe4a12c8fa9f34f2a9325fd127de4e047
Ddtester5 <dd5892631@gmail.com>
2025-03-19 12:34:57 +0000

    create express app

--- backand/src/index.ts
+++ backand/src/index.ts
@@ -1,1 +1,9 @@
-console.log(123);
+import express from 'express'
+
+const app = express()
+
+app.get('/',(req,res)=>{res.send('hi')})
+
+app.listen(3000,()=>{
+    console.warn('Listening on http://localhost:3000')
+})
